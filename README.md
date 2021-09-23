727976720d8c50b7302f3ae9500adf215f4315d3
RedHeadphone <khilawala92@gmail.com>
2021-09-23 20:31:53 +0530

    import grid from image half done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # path-finding
+
+Finds smallest path between 2 points in a grid using A* Algorithm
--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import pygame as p
 
-side=600
-numbox=20
-eachlen=side//numbox
-
-p.init()
-screen=p.display.set_mode((side,side+60))
-p.display.set_caption("Path Finding")
+import cv2,numpy,sys
 
 class Button:
     def __init__(self,x,y,st,c1,c2):
@@ -90,10 +84,14 @@ def checkn(k):
                 blocks[xn][yn].dfs=node.dfs+distancecalc((xn*eachlen,yn*eachlen),nodev)
         elif (xn*eachlen,yn*eachlen)==end:
             path.add(end)
+            ctr2=0
             while node!=blocks[start[0]//eachlen][start[1]//eachlen]:
                 path.add((node.coor))
                 node=node.parent
-                allrender()
+                ctr2+=1
+                if ctr2==int(renderspeed*0.7):
+                    ctr2=0
+                    allrender()
             mousef=None
         else:
             if 0<=xn<numbox and 0<=yn<numbox:
@@ -101,6 +99,17 @@ def checkn(k):
                 blocks[xn][yn].dfs=node.dfs+distancecalc((xn*eachlen,yn*eachlen),nodev)
                 blocks[xn][yn].dfe=distancecalc((xn*eachlen,yn*eachlen),(end[0],end[1]))
                 neighbour.add((xn*eachlen,yn*eachlen))
+
+print(sys.argv)
+side=600
+numbox=200
+eachlen=side//numbox
+renderspeed=50
+countertorefresh=0
+todrawgrid=False
+gridlinewidth=1
+thresholdvalue=0.5
+
 
 
 blocks=[[Point(j,i) for i in range(numbox)] for j in range(numbox)]
@@ -111,11 +120,48 @@ doneblock=set()
 neighbour=set()
 path=set()
 
+if len(sys.argv)>1:
+    img=cv2.imread(sys.argv[1],0)
+    img=cv2.resize(img,(numbox,numbox))
+
+    cv2.imshow("test",img)
+    def back(*args):
+        print("pressed")
+    cv2.createButton("Back",back,None,cv2.QT_PUSH_BUTTON,1)
+
+    
+    def on_trackbar(val):
+        print(val)
+
+    cv2.createTrackbar("threshold", "test" , 0, 100, on_trackbar)
+
+    cv2.createTrackbar("threshold", "test" , 0, 100, on_trackbar)
+
+    
+
+    
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+    
+    exit()
+
+    img = (img/255.).astype(numpy.float32)
+
+    for i in range(numbox):
+        for j in range(numbox):
+            if img[i][j]<thresholdvalue:
+                blocked.add((j*eachlen,i*eachlen))
+
+
+p.init()
+screen=p.display.set_mode((side,side+60))
+p.display.set_caption("Path Finding")
+
 def draw_grid():
     for x in range(1,numbox+1):
-        p.draw.line(screen,(0,0,0),(0,x*eachlen),(side,x*eachlen),2)
+        p.draw.line(screen,(0,0,0),(0,x*eachlen),(side,x*eachlen),gridlinewidth)
     for y in range(1,numbox+1):
-        p.draw.line(screen,(0,0,0),(y*eachlen,0),(y*eachlen,side),2)
+        p.draw.line(screen,(0,0,0),(y*eachlen,0),(y*eachlen,side),gridlinewidth)
 
 def allrender():
     screen.fill((255,255,255))
@@ -132,7 +178,8 @@ def allrender():
     
     for i in blocked:
         p.draw.rect(screen,(15,15,15),[(i[0],i[1]),(eachlen,eachlen)])
-    draw_grid()
+    if todrawgrid:
+        draw_grid()
     for b in Bs:
         b.check(mot0,mot1,mo[0])
         b.draw_button()
@@ -141,7 +188,9 @@ def allrender():
 mousef=None
 done=False
 Bs=[Button(10,610,"start point",(75,194,197),(52,132,152)),Button(120,610,"end point",(75,194,20),(52,132,30)),
-    Button(230,610,"block",(200,132,132),(200,75,75)),Button(340,610,"reset",(132,132,132),(99,75,75)),Button(490,610,"find path",(200,200,30),(200,100,30))]
+    Button(230,610,"block",(200,132,132),(200,75,75)),Button(340,610,"reset",(132,132,132),(99,75,75)),
+    Button(490,610,"find path",(200,200,30),(200,100,30))]
+
 while not done:
     for event in p.event.get():
         if event.type==p.QUIT:
@@ -175,6 +224,8 @@ while not done:
         else:
             neighbour.remove(min)
             checkn(min)
-
-    allrender()
+    countertorefresh+=1
+    if countertorefresh==renderspeed:
+        countertorefresh=0
+        allrender()
 p.quit()
